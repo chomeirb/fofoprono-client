188331c0609ad9d477f73a05b4b488a2fb5a69f0
Faustin <faustin.boitel@bordeaux-inp.fr>
2022-11-11 08:02:40 +0100

    title player filter

--- src/routes/ranking/Filter.svelte
+++ src/routes/ranking/Filter.svelte
@@ -3,10 +3,9 @@
 </script>
 
 <div class="flex flex-col gap-3 justify-start w-[90%] shadow-in mt-4 px-4 py-5 h-min">
-  <div class="flex flex-col w-full">
+  <div class="flex flex-col w-full" title="Vous pouvez entrer plusieurs joueurs en les séparant par une virgule">
     <label for="player">Joueurs</label>
     <input
-      title="Vous pouvez entrer plusieurs joueurs en les séparant par une virgule"
       id="player"
       type="text"
       class="bg-primary dark:bg-secondary text-secondary dark:text-primary rounded px-3 py-1 w-full h-10"
