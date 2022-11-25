0924338af56a16e05ab6fca05cd1765e415066c7
theogom <theo.gom.2000@gmail.com>
2022-11-25 12:07:26 +0100

    style: use vectorised logos

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -5,10 +5,8 @@ module.exports = {
   theme: {
     extend: {
       colors: {
-        // primary: "#121212",
         primary: "#3C3744",
         secondary: "#FBFFF1",
-        tertiary: "#3D52D5",
       },
       boxShadow: {
         'top': '0 -10px 15px -3px rgba(0, 0, 0, 0.2)',
