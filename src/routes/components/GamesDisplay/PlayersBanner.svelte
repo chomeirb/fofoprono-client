1ae5f304035ef041bd99d050f3e5793966144c86
Canleskis <91223490+Canleskis@users.noreply.github.com>
2022-11-29 00:46:01 +0100

    FIX: text position in ranking banner

--- src/routes/components/GamesDisplay/PlayersBanner.svelte
+++ src/routes/components/GamesDisplay/PlayersBanner.svelte
@@ -15,9 +15,9 @@
 
 <div class="flex h-min w-full flex-row items-center justify-between rounded-md border-[3px] border-primary px-10 py-3 shadow-in dark:border-secondary m12:px-[5%]">
 	<p class="w-[10%] text-xl text-primary dark:text-secondary m12:w-[25%] m6:text-base" title="rang">Rang</p>
-	<button class="w-[60%] text-xl text-primary hover:cursor-pointer hover:underline hover:opacity-70 dark:text-secondary m6:text-base" on:click={() => setSort(SortType.Name)}>
+	<button class="w-[60%] text-left text-xl text-primary hover:cursor-pointer hover:underline hover:opacity-70 dark:text-secondary m6:text-base" on:click={() => setSort(SortType.Name)}>
 		Nom {sortType == SortType.Name ? (sortAsc ? '▲' : '▼') : ''}
-	</button>
+    </button>
 	<button
 		class="w-[17%] text-left text-xl text-primary hover:cursor-pointer hover:underline hover:opacity-70 dark:text-secondary m12:hidden m6:text-base"
 		title="Scores exacts trouvé"
