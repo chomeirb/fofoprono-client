fd04dc7b665ed4b8d3a740908e9fa70fa354872e
Faustin <boitel.faustin@gmail.com>
2022-12-03 02:06:00 +0100

    FEAT(style): Button style on home page and prono page

--- src/routes/prono/+page.svelte
+++ src/routes/prono/+page.svelte
@@ -6,9 +6,9 @@
 
 <div class="flex h-full w-full flex-col items-center justify-start pt-3 m8:pt-0">
 	{#if !$session.data}
-		<div class="flex h-full flex-col items-center justify-center gap-10">
-			<p class="w-2/3 items-center text-center text-4xl font-bold">Vous devez vous connecter pour effectuer des pronostics !</p>
-			<a href="/login" class="rounded-3xl bg-primary bg-opacity-20 p-3 text-3xl font-semibold duration-200 hover:translate-x-5 dark:bg-white dark:bg-opacity-10">Connexion →</a>
+		<div class="flex h-full flex-col items-center justify-center text-4xl gap-10">
+			<p class="w-2/3 items-center text-center font-bold">Vous devez vous connecter pour effectuer des pronostics !</p>
+			<a href="/login" class="button">Connexion →</a>
 		</div>
 	{:else}
 		<div class="flex h-[10%] w-full max-w-8xl flex-row items-center justify-between px-3 text-4xl m12:px-3 m12:text-xl m8:h-[8%]">
@@ -16,7 +16,7 @@
 			<button
 				type="submit"
 				form="Pronos"
-				class="flex flex-row items-center gap-5 rounded-3xl bg-primary bg-opacity-20 py-2 px-5 text-primary duration-300 hover:translate-x-3 dark:bg-white dark:bg-opacity-10 dark:text-secondary">
+				class="flex flex-row items-center gap-5 button ">
 				<p class="m8:hidden">Valider</p>
 				<Checkmark height={25} /></button>
 		</div>
