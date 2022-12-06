fb451b69cd3d41b9fa0e7ed03191998b4181927b
Canleskis <91223490+Canleskis@users.noreply.github.com>
2022-12-06 15:48:12 +0100

    FEAT: appropriate button if not logged in in prono

--- src/routes/prono/+page.svelte
+++ src/routes/prono/+page.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
 	import Checkmark from '../components/icons/Checkmark.svelte';
+	import LoginArrow from '../components/icons/LoginArrow.svelte';
 	import { session } from '../store';
 	import Pronos from './Pronos.svelte';
 </script>
@@ -7,9 +8,17 @@
 <div class="flex h-full w-full flex-col items-center justify-start pt-3 m8:pt-0">
 	<div class="flex h-[10%] w-full max-w-8xl flex-row items-center justify-between px-3 text-4xl m12:px-3 m12:text-xl m8:h-[8%]">
 		<p class="font-bold m12:text-xl">PRONOSTIQUER</p>
-		<button type="submit" form="Pronos" class="button flex flex-row items-center gap-5 ">
-			<p class="m8:hidden">Valider</p>
-			<Checkmark height={25} /></button>
+		{#if $session.data}
+			<button type="submit" form="Pronos" class="button flex flex-row items-center gap-5">
+				<p class="m8:hidden">Valider</p>
+				<Checkmark height={25} />
+			</button>
+		{:else}
+			<a href="/login" class="button flex flex-row items-center gap-5">
+				<p class="m8:hidden">Pronostiquer</p>
+				<LoginArrow height={45} />
+			</a>
+		{/if}
 	</div>
 	<div class="h-[83%] w-full max-w-8xl px-3 m12:mt-2 m12:px-0 m8:mt-0">
 		<Pronos />
