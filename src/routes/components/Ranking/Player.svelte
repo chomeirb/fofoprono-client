d31f46e54e0235c0bd5105f92c59b3efb1e935a2
Canleskis <91223490+Canleskis@users.noreply.github.com>
2022-12-07 06:32:00 +0100

    FIX: ranking not highlighting logged user

--- src/routes/components/Ranking/Player.svelte
+++ src/routes/components/Ranking/Player.svelte
@@ -2,11 +2,13 @@
 	import { UserType, type RankedUser } from '$lib/types/player';
 
 	export let player: RankedUser;
+
+	$: console.log(player.user_type === UserType.Current);
 </script>
 
 <a
-	class={`flex w-full flex-row items-center justify-between rounded-md border-2 border-primary border-opacity-70 px-5 py-3 shadow-xl hover:opacity-60 dark:border-secondary dark:border-opacity-30 m6:py-1 ${
-		player.user_type === UserType.Current ? 'bg-primary bg-opacity-20 dark:bg-white dark:bg-opacity-10' : ''
+	class={`flex w-full flex-row items-center justify-between rounded-md border-2 border-primary border-opacity-70 px-5 py-3 shadow-xl hover:opacity-60 dark:border-secondary dark:border-opacity-70 m6:py-1 ${
+		player.user_type === UserType.Current ? 'bg-primary bg-opacity-10 dark:bg-white dark:bg-opacity-10' : ''
 	}`}
 	href="/prono/{player.name}?from=2022-11-20">
 	<p class="h-full w-[10%] text-xl text-primary dark:text-secondary m6:text-base">{player.rank}</p>
